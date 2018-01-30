9447163a8c7e615709b266c104638fd619d9ed1f
Peter Burns <peterburnshicks@gmail.com>
2018-01-30 00:19:13 -0500

    added web page layout and frontend logic, added json parsing to server post route

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,10 @@ var friendArr = [{
     name:'Pete',
     photo:'https://placehold.it/250X250',
     scores:[1,2,3,4,5,1,2,3,4,5]
+},{
+    name:'Mark',
+    photo:'https://placehold.it/250X250',
+    scores:[2,3,4,5,1,2,3,4,5,1]
 }];
 
 module.exports = friendArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-
 var friendArr = require('../data/friends');
 
 module.exports = function (app) {
@@ -8,7 +7,21 @@ module.exports = function (app) {
     });
 
     app.post('/api/friends', function (req, res) {
+        var bestMatch;
+        var leastDiff = 100; 
+        var newFriend = typeof(req.body.scores) == 'string' ? JSON.parse(req.body.scores) : req.body.scores;
+        friendArr.forEach(function(friend){
+            var difference = newFriend.map(function(val, ind){
+                return Math.abs(val-friend.scores[ind]);
+            }).reduce(function(a,b){
+                return a+b;
+            });
+            if(leastDiff > difference){
+                leastDiff = difference;
+                bestMatch = friend;
+            }
+        });
         friendArr.push(req.body);
-        res.json(true);
+        res.json(bestMatch);
     });
 };
