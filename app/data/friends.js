e3b0c10de0b31b51cc45776129da2590f3dadda7
Peter Burns <peterburnshicks@gmail.com>
2018-01-30 14:41:05 -0500

    added friends and photo urls

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,27 @@
 var friendArr = [{
     name:'Pete',
-    photo:'https://placehold.it/250X250',
+    photo:'https://avatars0.githubusercontent.com/u/19544844?s=400&u=dc4e16b0f6d24a3e6ff067acc84d23c9faa080ee&v=4',
+    scores:[2,1,1,5,1,3,5,5,3,2]
+},{
+    name:'Steve',
+    photo:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE5NDg0MDU0NTIzODQwMDE1/steven-jobs-9354805-2-402.jpg',
     scores:[1,2,3,4,5,1,2,3,4,5]
 },{
     name:'Mark',
-    photo:'https://placehold.it/250X250',
+    photo:'https://specials-images.forbesimg.com/imageserve/59d5062131358e542c034eb7/416x416.jpg?background=000000&cropX1=419&cropX2=1409&cropY1=53&cropY2=1044',
     scores:[2,3,4,5,1,2,3,4,5,1]
+},{
+    name:'Andy',
+    photo:'http://harlemtoys.com/wp-content/uploads/2016/02/andy-toy-story.jpg',
+    scores:[2,2,3,4,1,2,3,3,4,1]
+},{
+    name:'Buzz',
+    photo:'https://upload.wikimedia.org/wikipedia/en/b/b4/Buzz_Lightyear.png',
+    scores:[1,1,1,1,1,1,1,1,1,1]
+},{
+    name:'Luke',
+    photo:'https://vignette.wikia.nocookie.net/starwars/images/d/d9/Luke-rotjpromo.jpg/revision/latest/scale-to-width-down/350?cb=20091030151422',
+    scores:[4,3,2,1,5,5,3,4,2,1]
 }];
 
 module.exports = friendArr;
